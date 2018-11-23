880e43e6e212c237d691a59fba98c6d7f03854c7
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-23 15:28:49 +0800

    working

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.5.0;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+    constructor() public {
+      owner = msg.sender;
+    }
 
   modifier restricted() {
     if (msg.sender == owner) _;
